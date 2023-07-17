1e36369d4c35dc50e705b5facd4233a1f7bc0848
Kieran-Lock <Kieran.Lock@outlook.com>
2023-07-17 19:39:58 +0100

    Solved kahoot challenge

--- autokahoot/kahoot.py
+++ autokahoot/kahoot.py
@@ -3,6 +3,10 @@ from dataclasses import dataclass
 from contextlib import contextmanager
 from typing import Iterator
 from time import time
+from py_mini_racer import MiniRacer
+from base64 import b64decode
+from re import split as re_split
+from itertools import cycle
 from requests import Response, session as requests_session
 from .event import Event
 
@@ -15,12 +19,14 @@ class Kahoot:
 
     @contextmanager
     def connect(self) -> Iterator[Kahoot]:
-        session_response = self.reserve_session()
-        print(session_response, session_response.json(), sep="\n")
+        response = self.reserve_session()
+        session_id = self.solve_challenge(
+            response.json().get("challenge"), response.headers.get("x-kahoot-session-token")
+        )
         yield self
 
     def events(self) -> Iterator[Event]:
-        yield Event("Not Implemented")
+        yield Event(f"Developing solution for game with ID '{self.game_id}'")
 
     def reserve_session(self) -> Response:
         session = requests_session()
@@ -29,3 +35,14 @@ class Kahoot:
         if response.status_code != 200:
             raise ValueError(f"No game exists with pin '{self.pin}'") from None
         return response
+
+    @staticmethod
+    def solve_challenge(challenge: str, session_token: str) -> str:
+        session_token = b64decode(session_token).decode("utf-8", "strict")
+        challenge = re_split("[{};]", challenge.replace("\t", "", -1).encode("ascii", "ignore").decode("utf-8"))
+        solution = MiniRacer().eval(
+            f"{challenge[1]}{{{challenge[2]};return message.replace(/./g, function(char, position)"
+            f"{{{challenge[7]};}})}};{challenge[0]}"
+        )
+        return "".join(chr(ord(session_token_character) ^ ord(solution_character))
+                       for session_token_character, solution_character in zip(session_token, cycle(solution)))
