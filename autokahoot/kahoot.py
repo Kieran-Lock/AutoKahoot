67142b007960cd9aea7ef998c9decb329f59bbac
Kieran-Lock <Kieran.Lock@outlook.com>
2023-07-17 19:04:49 +0100

    Created session reservations

--- autokahoot/kahoot.py
+++ autokahoot/kahoot.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from dataclasses import dataclass
 from contextlib import contextmanager
 from typing import Iterator
+from time import time
+from requests import Response, session as requests_session
 from .event import Event
 
 
@@ -13,7 +15,17 @@ class Kahoot:
 
     @contextmanager
     def connect(self) -> Iterator[Kahoot]:
+        session_response = self.reserve_session()
+        print(session_response, session_response.json(), sep="\n")
         yield self
 
     def events(self) -> Iterator[Event]:
         yield Event("Not Implemented")
+
+    def reserve_session(self) -> Response:
+        session = requests_session()
+        url = f"https://play.kahoot.it/reserve/session/{self.pin}/?{int(time())}"
+        response = session.get(url)
+        if response.status_code != 200:
+            raise ValueError(f"No game exists with pin '{self.pin}'") from None
+        return response
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def get_inputs() -> tuple[str, str, str]:
 
 
 def main() -> None:
-    username, pin, game_id = get_inputs()
+    # username, pin, game_id = get_inputs()
+    username, pin, game_id = "Bot", "1355940", "9983b771-65bc-4175-af56-d647d3cabc8d"  # Testing game details
     with Kahoot(username, pin, game_id).connect() as kahoot:
         for event in kahoot.events():
             print(event)
