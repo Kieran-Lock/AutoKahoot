47b2ef63f2efad4fa90c5c5e9c262f8716ef10ba
Kieran-Lock <Kieran.Lock@outlook.com>
2023-07-18 20:59:05 +0100

    Fixed connection issues

--- autokahoot/kahoot.py
+++ autokahoot/kahoot.py
@@ -7,9 +7,23 @@ from py_mini_racer import MiniRacer
 from base64 import b64decode
 from re import split as re_split
 from itertools import cycle
-from aiocometd_noloop import Client as CometDClient
+from aiocometd_noloop import Extension, Client as CometDClient
 from requests import Response, session as requests_session
 from .event import Event
+
+
+class LoggingExtension(Extension):
+    async def incoming(self, payload, headers=None):
+        if payload[0].get("channel").startswith("/meta"):
+            return payload
+        print("Incoming:", payload, end="\n\n")
+        return payload
+
+    async def outgoing(self, payload, headers):
+        if isinstance(payload[0].get("channel"), MetaChannel):
+            return payload
+        print("Outgoing:", payload, end="\n\n")
+        return payload
 
 
 @dataclass(frozen=True, slots=True)
@@ -25,7 +39,7 @@ class Kahoot:
             response.json().get("challenge"), response.headers.get("x-kahoot-session-token")
         )
         url = f"wss://play.kahoot.it/cometd/{self.pin}/{session_id}"
-        async with CometDClient(url, ssl=True) as client:
+        async with CometDClient(url, ssl=True, extensions=[LoggingExtension()]) as client:
             yield client
 
     async def events(self, client: CometDClient) -> AsyncIterator[Event]:
@@ -35,9 +49,35 @@ class Kahoot:
             "/service/controller",
             {
                 "host": "kahoot.it", "gameid": self.pin,
-                "captchaToken": "KAHOOT_TOKEN_frkdbxs3k7nf741hbvf=", "name": self.username, "type": "login"
+                "name": self.username, "type": "login",
+                "content": "{\"device\":{\"userAgent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82\",\"screen\":{\"width\":1920,\"height\":1080}}}"
             }
         )
+        async for message in client:
+            if message.get("data", {}).get("type") == "loginResponse":
+                break
+
+        await client.publish(
+            "/service/controller",
+            {
+                "type": "message",
+                "gameid": int(self.pin),
+                "host": "kahoot.it",
+                "content": "{\"usingNamerator\":false}",
+                "id": 16
+            }
+        )
+        # # input("b\n")
+        # await client.publish(
+        #     "/service/controller",
+        #     {
+        #         "id": 46,
+        #         "type": "message",
+        #         "gameid": "3647098",
+        #         "host": "kahoot.it",
+        #         "content": {"avatar": {"type": 2200, "item": 1550}}
+        #     }
+        # )
         async for client_message in client:
             yield Event(client_message.get("data"))
 
--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from autokahoot import Kahoot
 from asyncio import run
+from threading import Thread
 
 
 def get_inputs() -> tuple[str, str, str]:
-    username = input("Username:\t")
-    pin = input("Pin:\t")
-    game_id = input("Game ID:\t")
-    return username, pin, game_id
+    # username = input("Username:\t")
+    # pin = input("Pin:\t")
+    # game_id = input("Game ID:\t")
+    # return username, pin, game_id
+    return (
+        "Pickles9",
+        "4642201",
+        "739c6780-b383-463f-9a9c-c9d1d9214fab"
+    )
 
 
 async def main() -> None:
-    # username, pin, game_id = get_inputs()
-    username, pin, game_id = "Bot", "2864568", "9983b771-65bc-4175-af56-d647d3cabc8d"  # Testing game details
+    username, pin, game_id = get_inputs()
     kahoot = Kahoot(username, pin, game_id)
     async with kahoot.connect() as client:
         async for event in kahoot.events(client):
-            print(event)
+            pass
+            # print(event)
+
+
+def run_program():
+    run(main())
 
 
 if __name__ == "__main__":
-    run(main())
+    t = Thread(target=run_program)
+    t.start()
+    t.join()
